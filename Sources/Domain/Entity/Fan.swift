ddf33e550840a7148a528c634556172bea4eb001
Yuta Saito <kateinoigakukun@gmail.com>
2020-10-19 00:43:44 +0900

    Remove comment outed code

--- Sources/Domain/Entity/Fan.swift
+++ Sources/Domain/Entity/Fan.swift
@@ -1,22 +1,2 @@
-//
-//  File.swift
-//
-//
-//  Created by Masato TSUTSUMI on 2020/10/08.
-//
-
-import Foundation
-
-//public struct Fan: Codable {
-//    public let id: UUID
-//    public var name: String
-//    public var biography: String?
-//    public var thumbnailURL: String?
-//
-//    public init(id: UUID, name: String, biography: String?, thumbnailURL: String?) {
-//        self.id = id
-//        self.name = name
-//        self.biography = biography
-//        self.thumbnailURL = thumbnailURL
-//    }
-//}
+public struct Fan: Codable {
+}
