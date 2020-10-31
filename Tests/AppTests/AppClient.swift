2162d7d0f0d2e6d54d863fbe15d6466140447b51
Yuta Saito <kateinoigakukun@gmail.com>
2020-10-31 17:36:06 +0900

    Refactoring test client

---
+++ Tests/AppTests/AppClient.swift
@@ -0,0 +1,78 @@
+import Endpoint
+import StubKit
+import Vapor
+
+class AppUser {
+    private let cognito: CognitoClient
+    private let userName: String
+    let token: String
+    init(userName: String, cognito: CognitoClient) {
+        self.userName = userName
+        self.cognito = cognito
+        let user = try! cognito.createToken(userName: userName).wait()
+        self.token = user.token
+    }
+    deinit {
+        try! cognito.destroyUser(userName: userName).wait()
+    }
+}
+
+class AppClient {
+    let encoder: JSONEncoder = {
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        return encoder
+    }()
+
+    private let app: Application
+    private let cognito: CognitoClient
+    init(application: Application, cognito: CognitoClient) {
+        self.app = application
+        self.cognito = cognito
+    }
+
+    func makeHeaders(for user: AppUser) -> HTTPHeaders {
+        var headers = HTTPHeaders()
+        headers.add(name: .authorization, value: "Bearer \(user.token)")
+        headers.add(name: .contentType, value: HTTPMediaType.json.serialize())
+        return headers
+    }
+
+    func createUser(
+        name: String = UUID().uuidString,
+        role: RoleProperties = .artist(Artist(part: "vocal"))
+    ) throws -> AppUser {
+        let user = AppUser(userName: UUID().uuidString, cognito: cognito)
+        let headers = makeHeaders(for: user)
+        let body = Endpoint.Signup.Request(name: name, role: role)
+        let bodyData = try ByteBuffer(data: encoder.encode(body))
+        try app.test(.POST, "users/signup", headers: headers, body: bodyData)
+        return user
+    }
+
+    func createGroup(body: CreateGroup.Request = try! Stub.make(), with user: AppUser) throws
+        -> Endpoint.Group
+    {
+        let bodyData = try ByteBuffer(data: encoder.encode(body))
+        var createdGroup: Endpoint.Group!
+        try app.test(.POST, "groups", headers: makeHeaders(for: user), body: bodyData) { res in
+            createdGroup = try res.content.decode(CreateGroup.Response.self)
+        }
+        return createdGroup
+    }
+
+    func createInvitation(group: Endpoint.Group, with user: AppUser) throws
+        -> Endpoint.InviteGroup.Invitation
+    {
+        let body = try! Stub.make(InviteGroup.Request.self) {
+            $0.set(\.groupId, value: group.id)
+        }
+        let bodyData = try ByteBuffer(data: encoder.encode(body))
+        var createdInvitation: Endpoint.InviteGroup.Invitation!
+        try app.test(.POST, "groups/invite", headers: makeHeaders(for: user), body: bodyData) {
+            res in
+            createdInvitation = try res.content.decode(InviteGroup.Response.self)
+        }
+        return createdInvitation
+    }
+}
--- Tests/AppTests/Controllers/GroupControllerTests.swift
+++ Tests/AppTests/Controllers/GroupControllerTests.swift
@@ -7,137 +7,103 @@ import XCTVapor
 
 class GroupControllerTests: XCTestCase {
     var app: Application!
+    var appClient: AppClient!
     override func setUp() {
         app = Application(.testing)
         DotEnvFile.load(path: dotEnvPath.path)
         XCTAssertNoThrow(try configure(app))
+        appClient = AppClient(application: app, cognito: CognitoClient())
     }
 
     override func tearDown() {
         app.shutdown()
     }
 
-    func testCreateBand_Invite_Join() throws {
+    func testInviteForNonExistingGroup() throws {
+        let user = try appClient.createUser(role: .artist(Artist(part: "vocal")))
+        let body = try! Stub.make(InviteGroup.Request.self) {
+            let fakeGroup = UUID()
+            $0.set(\.groupId, value: fakeGroup.uuidString)
+        }
+        let bodyData = try ByteBuffer(data: appClient.encoder.encode(body))
+        try app.test(
+            .POST, "groups/invite", headers: appClient.makeHeaders(for: user),
+            body: bodyData
+        ) { res in
+            XCTAssertNotEqual(res.status, .ok, res.body.string)
+        }
+    }
+
+    func testInviteForNonMemberGroup() throws {
+        let user = try appClient.createUser(role: .artist(Artist(part: "vocal")))
+        let createdGroup = try appClient.createGroup(with: user)
+        let nonMemberUser = try appClient.createUser()
+
+        let body = try! Stub.make(InviteGroup.Request.self) {
+            $0.set(\.groupId, value: createdGroup.id)
+        }
+
+        let bodyData = try ByteBuffer(data: appClient.encoder.encode(body))
+        // Try to create an invitation for non-member group
+        try app.test(
+            .POST, "groups/invite", headers: appClient.makeHeaders(for: nonMemberUser),
+            body: bodyData
+        ) { res in
+            XCTAssertNotEqual(res.status, .ok, res.body.string)
+        }
+    }
+
+    func testGetGroup() throws {
+        let user = try appClient.createUser(role: .artist(Artist(part: "vocal")))
+        let headers = appClient.makeHeaders(for: user)
+        let createdGroup = try appClient.createGroup(with: user)
+
+        try app.test(.GET, "groups/\(createdGroup.id)", headers: headers) { res in
+            XCTAssertEqual(res.status, .ok, res.body.string)
+        }
+    }
+
+    func testJoinWithInvalidInvitation() throws {
+        let user = try appClient.createUser(role: .artist(Artist(part: "vocal")))
+        let headers = appClient.makeHeaders(for: user)
+        _ = try appClient.createGroup(with: user)
+
+        let body = try! Stub.make(JoinGroup.Request.self) {
+            let fakeInvitation = UUID()
+            $0.set(\.invitationId, value: fakeInvitation.uuidString)
+        }
+        let bodyData = try ByteBuffer(data: appClient.encoder.encode(body))
+        // Try to join with invalid invitation
+        try app.test(.POST, "groups/join", headers: headers, body: bodyData) { res in
+            XCTAssertEqual(res.status, .badRequest)
+        }
+    }
+
+    func testJoinTwice() throws {
         // try to create without login
         try app.test(.POST, "groups") { res in
             XCTAssertEqual(res.status, .unauthorized)
         }
-        let client = CognitoClient()
-        let dummyCognitoUserName = UUID().uuidString
-        let dummyUser = try client.createToken(userName: dummyCognitoUserName).wait()
-        defer { try! client.destroyUser(userName: dummyCognitoUserName).wait() }
 
-        let encoder = JSONEncoder()
-        encoder.dateEncodingStrategy = .iso8601
+        let user = try appClient.createUser(role: .artist(Artist(part: "vocal")))
+        let encoder = appClient.encoder
 
-        var headers = HTTPHeaders()
-        headers.add(name: .authorization, value: "Bearer \(dummyUser.token)")
-        headers.add(name: .contentType, value: HTTPMediaType.json.serialize())
-        do {
-            let dummyUserName = UUID().uuidString
-            let body = Endpoint.Signup.Request(
-                name: dummyUserName, role: .artist(Artist(part: "vocal")))
-            let bodyData = try ByteBuffer(data: JSONEncoder().encode(body))
+        let headers = appClient.makeHeaders(for: user)
+        let createdGroup = try appClient.createGroup(with: user)
+        let createdInvitation = try appClient.createInvitation(group: createdGroup, with: user)
 
-            try app.test(.POST, "users/signup", headers: headers, body: bodyData) { res in
-                XCTAssertEqual(res.status, .ok)
-                let responseBody = try res.content.decode(Signup.Response.self)
-                XCTAssertEqual(responseBody.name, dummyUserName)
-            }
+        let body = try! Stub.make(JoinGroup.Request.self) {
+            $0.set(\.invitationId, value: createdInvitation.id)
+        }
+        let bodyData = try ByteBuffer(data: encoder.encode(body))
+        try app.test(.POST, "groups/join", headers: headers, body: bodyData) { res in
+            XCTAssertEqual(res.status, .ok)
+            _ = try res.content.decode(JoinGroup.Response.self)
         }
 
-        var createdGroup: Endpoint.Group!
-        do {
-            // Create a group
-            let body = try! Stub.make(CreateGroup.Request.self) {
-                $0.set(\.name, value: "Super unique lucky name")
-            }
-            let bodyData = try ByteBuffer(data: encoder.encode(body))
-            try app.test(.POST, "groups", headers: headers, body: bodyData) { res in
-                XCTAssertEqual(res.status, .ok)
-                let responseBody = try res.content.decode(CreateGroup.Response.self)
-                XCTAssertEqual(responseBody.name, body.name)
-                createdGroup = responseBody
-            }
-        }
-
-        do {
-            let body = try! Stub.make(InviteGroup.Request.self) {
-                let fakeGroup = UUID()
-                $0.set(\.groupId, value: fakeGroup.uuidString)
-            }
-            let bodyData = try ByteBuffer(data: encoder.encode(body))
-            // Try to create an invitation for non-existing group
-            try app.test(.POST, "groups/invite", headers: headers, body: bodyData) { res in
-                XCTAssertNotEqual(res.status, .ok, res.body.string)
-            }
-        }
-
-        do {
-            let nonMemberUserName = UUID().uuidString
-            let nonMemberUser = try client.createToken(userName: nonMemberUserName).wait()
-            defer { try! client.destroyUser(userName: nonMemberUserName).wait() }
-
-            var headers = HTTPHeaders()
-            headers.add(name: .authorization, value: "Bearer \(nonMemberUser.token)")
-            headers.add(name: .contentType, value: HTTPMediaType.json.serialize())
-
-            let body = try! Stub.make(InviteGroup.Request.self) {
-                $0.set(\.groupId, value: createdGroup.id)
-            }
-
-            let bodyData = try ByteBuffer(data: encoder.encode(body))
-            // Try to create an invitation for non-member group
-            try app.test(.POST, "groups/invite", headers: headers, body: bodyData) { res in
-                XCTAssertNotEqual(res.status, .ok, res.body.string)
-            }
-        }
-
-        var createdInvitation: Endpoint.InviteGroup.Invitation!
-        do {
-            let body = try! Stub.make(InviteGroup.Request.self) {
-                $0.set(\.groupId, value: createdGroup.id)
-            }
-            let bodyData = try ByteBuffer(data: encoder.encode(body))
-            try app.test(.POST, "groups/invite", headers: headers, body: bodyData) { res in
-                XCTAssertEqual(res.status, .ok, res.body.string)
-                createdInvitation = try res.content.decode(InviteGroup.Response.self)
-            }
-        }
-
-        do {
-            let body = try! Stub.make(JoinGroup.Request.self) {
-                $0.set(\.invitationId, value: createdInvitation.id)
-            }
-            let bodyData = try ByteBuffer(data: encoder.encode(body))
-            try app.test(.POST, "groups/join", headers: headers, body: bodyData) { res in
-                XCTAssertEqual(res.status, .ok)
-                _ = try res.content.decode(JoinGroup.Response.self)
-            }
-
-            // Try to join again with the same invitation
-            try app.test(.POST, "groups/join", headers: headers, body: bodyData) { res in
-                XCTAssertEqual(res.status, .badRequest)
-            }
-        }
-
-        do {
-            let body = try! Stub.make(JoinGroup.Request.self) {
-                let fakeInvitation = UUID()
-                $0.set(\.invitationId, value: fakeInvitation.uuidString)
-            }
-            let bodyData = try ByteBuffer(data: encoder.encode(body))
-            // Try to join with invalid invitation
-            try app.test(.POST, "groups/join", headers: headers, body: bodyData) { res in
-                XCTAssertEqual(res.status, .badRequest)
-            }
-        }
-
-        do {
-            // Try to join with invalid invitation
-            try app.test(.GET, "groups/\(createdGroup.id)", headers: headers) { res in
-                XCTAssertEqual(res.status, .ok, res.body.string)
-            }
+        // Try to join again with the same invitation
+        try app.test(.POST, "groups/join", headers: headers, body: bodyData) { res in
+            XCTAssertEqual(res.status, .badRequest)
         }
     }
 }
