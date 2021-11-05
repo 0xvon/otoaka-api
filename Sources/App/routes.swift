c7c7a3c68b22f053b97e613d4ad8dad39de3d1d8
masatsch <kingmasatojames@gmail.com>
2021-11-05 16:32:23 +0900

    allow cors

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,6 +7,14 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.routes.defaultMaxBodySize = "500kb"
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let corsMiddleware = CORSMiddleware(configuration: corsConfiguration)
+    app.middleware.use(corsMiddleware)
+    
     app.get { _ in
         "It works!"
     }
