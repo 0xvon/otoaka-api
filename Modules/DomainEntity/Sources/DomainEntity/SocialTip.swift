bbb04350bf601c514d1fdd5a4f2ff57c14a62293
masatsch <kingmasatojames@gmail.com>
2021-12-27 11:46:28 +0900

    SocialTip DomainEntity

---
+++ Modules/DomainEntity/Sources/DomainEntity/SocialTip.swift
@@ -0,0 +1,61 @@
+import Foundation
+
+public struct SocialTip: Codable, Identifiable, Equatable {
+    public typealias ID = Identifier<Self>
+    public let id: ID
+    public var user: User
+    public var tip: Int
+    public var type: SocialTipType
+    public var thrownAt: Date
+    
+    public init(
+        id: SocialTip.ID,
+        user: User,
+        tip: Int,
+        type: SocialTipType,
+        thrownAt: Date
+    ) {
+        self.id = id
+        self.user = user
+        self.tip = tip
+        self.type = type
+        self.thrownAt = thrownAt
+    }
+}
+
+public enum SocialTipType: Codable, Equatable {
+    case group(Group)
+    case live(Live)
+    
+    enum CodingKeys: CodingKey {
+        case kind, value
+    }
+    
+    enum Kind: String, Codable {
+        case group, live
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let kind = try container.decode(Kind.self, forKey: .kind)
+        switch kind {
+        case .group:
+            self = try .group(container.decode(Group.self, forKey: .value))
+        case .live:
+            self = try .live(container.decode(Live.self, forKey: .value))
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        switch self {
+        case let .group(group):
+            try container.encode(Kind.group, forKey: .kind)
+            try container.encode(group, forKey: .value)
+        case let .live(live):
+            try container.encode(Kind.live, forKey: .kind)
+            try container.encode(live, forKey: .value)
+        }
+    }
+}
